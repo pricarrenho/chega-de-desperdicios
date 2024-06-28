66f8b8f160319b12a382cf14f0e29c909b35d576
Priscilla <priscillaonil@gmail.com>
2024-06-28 18:06:52 -0300

    feat: created header and layout folder

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import Header from "../Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import RootLayout from "@/components/Layout";
+
+export default function Home() {
+  return (
+    <RootLayout>
+      <div>Home</div>
+    </RootLayout>
+  );
+}
