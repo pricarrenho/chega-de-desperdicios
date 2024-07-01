bbbf35e005f4f53eab275b6d7178e97b111fdf1f
Priscilla <priscillaonil@gmail.com>
2024-07-01 19:25:25 -0300

    style: active tab in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const router = useRouter();
   const hoverLink =
     "hover:text-lime-400 transition duration-150 hover:ease-in p-1";
+
+  const isActive = (pathname: string) => {
+    return router.pathname === pathname ? "text-lime-400" : "";
+  };
 
   return (
     <header className="bg-lime-800">
@@ -11,14 +17,14 @@ export function Header() {
 
         <nav>
           <ul className="flex gap-3">
-            <li className={hoverLink}>
-              <Link href={"/sobre"}>Sobre</Link>
+            <li className={`${hoverLink} ${isActive("/sobre")}`}>
+              <Link href="/sobre">Sobre</Link>
             </li>
-            <li className={hoverLink}>
-              <Link href={"/receitas"}>Receitas</Link>
+            <li className={`${hoverLink} ${isActive("/receitas")}`}>
+              <Link href="/receitas">Receitas</Link>
             </li>
-            <li className={hoverLink}>
-              <Link href={"/hortas"}>Hortas</Link>
+            <li className={`${hoverLink} ${isActive("/hortas")}`}>
+              <Link href="/hortas">Hortas</Link>
             </li>
           </ul>
         </nav>
