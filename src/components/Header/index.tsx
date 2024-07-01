fd55add49c4a760e25fa81dda70b300a332f3b36
Priscilla <priscillaonil@gmail.com>
2024-07-01 19:05:08 -0300

    feat: add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,28 @@
-export default function Header() {
-  return <header>Header</header>;
+import Link from "next/link";
+
+export function Header() {
+  const hoverLink =
+    "hover:text-lime-400 transition duration-150 hover:ease-in p-1";
+
+  return (
+    <header className="bg-lime-800">
+      <div className="container mx-auto py-3 flex justify-between items-center text-gray-50">
+        <div>LOGO</div>
+
+        <nav>
+          <ul className="flex gap-3">
+            <li className={hoverLink}>
+              <Link href={"/sobre"}>Sobre</Link>
+            </li>
+            <li className={hoverLink}>
+              <Link href={"/receitas"}>Receitas</Link>
+            </li>
+            <li className={hoverLink}>
+              <Link href={"/hortas"}>Hortas</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
