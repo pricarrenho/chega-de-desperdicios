c7564ab0efcbd7946b883f3d92b31a6b99358471
Priscilla <priscillaonil@gmail.com>
2024-08-26 17:00:16 -0300

    feat: add favicon and logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Whisper:wght@400&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="favicon.ico" sizes="any" />
       </Head>
       <body>
         <Main />
